0ec534f6f914480b6eb6a12e5360842c80c23b56
Worawat Wijarn <worawat.wijarn@gmail.com>
2019-07-28 10:53:46 +0700

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# openssl aes-256-cbc -K $encrypted_e07e39bfdc1d_key -iv $encrypted_e07e39bfdc1d_iv -in scripts/travis.gpg.enc -out travis.gpg -d
+openssl aes-256-cbc -K $encrypted_e07e39bfdc1d_key -iv $encrypted_e07e39bfdc1d_iv -in scripts/travis.gpg.enc -out travis.gpg -d
 ./gradlew monjam-core:publish -PsonatypeUsername=${SONATYPE_USERNAME} -PsonatypePassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=travis.gpg
 ./gradlew monjam-gradle-plugin:publishPlugins -Pgradle.publish.key=$GRADLE_KEY -Pgradle.publish.secret=$GRADLE_SECRET
